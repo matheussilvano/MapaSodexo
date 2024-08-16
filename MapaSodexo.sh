97215e1180e56763393fa01caccff053bd0cc669
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-08-16 11:17:37 -0300

    Update MapaSodexo.sh

--- MapaSodexo.sh
+++ MapaSodexo.sh
@@ -9,7 +9,7 @@ read -p "Informe o EC (código de afiliado com 9 dígitos): " EcSodexo
 # Procura o EC e informa a caixa postal (ID associado)
 CaixaPostal=$(grep "^$EcSodexo" /home/skyline/scripts/sodexho/Mapa_Clientes_Sodexo | awk '{print $2}')
 
-echo "Caixa Postal para o EC $EcSodexo: $CaixaPostal"
+read -p "A caixa postal cadastrada no mapa eh "$CaixaPostal", deseja realizar a troca? [S/N]: " ConfirmaTroca
 
 # Criacao de backup
 cp  -pv Mapa_Clientes_Sodexo BackupMapa
