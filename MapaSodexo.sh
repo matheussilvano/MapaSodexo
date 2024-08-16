44dca12c2888c6b62bcd276b0992378db38db255
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-08-16 12:16:07 -0300

    Update MapaSodexo.sh

--- MapaSodexo.sh
+++ MapaSodexo.sh
@@ -2,18 +2,34 @@
 
 DataHoje=$(date +%d%m%Y)
 BackupMapa="Mapa_Clientes_Sodexo_${DataHoje}_backup"
+MapaSodexo=/home/skyline/scripts/sodexho/Mapa_Clientes_Sodexo
 
 # Realiza a leitura do EC
 read -p "Informe o EC (código de afiliado com 9 dígitos): " EcSodexo
 
 # Procura o EC e informa a caixa postal (ID associado)
-CaixaPostal=$(grep "^$EcSodexo" /home/skyline/scripts/sodexho/Mapa_Clientes_Sodexo | awk '{print $2}')
+CaixaPostal=$(grep "^$EcSodexo" "$MapaSodexo" | awk '{print $2}')
 
 read -p "A caixa postal cadastrada no mapa eh "$CaixaPostal", deseja realizar a troca? [S/N]: " ConfirmaTroca
 
 # Criacao de backup
-cp  -pv Mapa_Clientes_Sodexo BackupMapa
+cp -pv "$MapaSodexo" /home/skyline/scripts/sodexho/$BackupMapa
 echo "Backup criado! -> $BackupMapa"
+
+if [[ "$ConfirmaTroca" =~ ^[Ss]$ ]]; then
+    read -p "Informe a nova caixa postal: " NovaCaixaPostal
+    # Faz a substituição no arquivo original
+    sed -i "s/^$EcSodexo .*/$EcSodexo $NovaCaixaPostal/" /home/skyline/scripts/sodexho/Mapa_Clientes_Sodexo
+    echo "Caixa postal atualizada para $NovaCaixaPostal"
+else
+    echo "Nenhuma alteração foi feita."
+fi
+
+
+
+
+
+
 
 
 
